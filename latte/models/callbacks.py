92680a769dfbb63880d63feff15010deda62c60b
Anej Svete <anej.svete@protonmail.com>
2022-05-17 11:07:16 +0200

    Implemented full testing of MINE. All tests pass but most take a long time.

--- latte/models/callbacks.py
+++ latte/models/callbacks.py
@@ -1,15 +1,29 @@
+import copy
+from collections import defaultdict
+
 import pytorch_lightning as pl
 
+from latte.models import mine
 
-# TODO (Anej)
+
 class MetricTracker(pl.Callback):
+    """A callback to track all the epoch-level metrics logged."""
+
     def __init__(self):
-        self.collection = []
+        self.epoch_outputs = []
 
-    def on_validation_batch_end(self, trainer, module, outputs):
-        vacc = outputs["val_acc"]
-        self.collection.append(vacc)
+    def on_validation_epoch_end(self, trainer: pl.Trainer, module: pl.LightningModule) -> None:
+        self.epoch_outputs.append(copy.deepcopy(trainer.logged_metrics))
 
-    def on_validation_epoch_end(self, trainer, module):
-        elogs = trainer.logged_metrics
-        self.collection.append(elogs)
+
+class WeightsTracker(pl.Callback):
+    """A callback for keeping track of the weights throughout training.
+    Used for unit testing."""
+
+    def __init__(self):
+        self.epoch_weights = defaultdict(list)
+
+    def on_validation_epoch_end(self, trainer: pl.Trainer, module: mine.MINE) -> None:
+        for ii, layer in enumerate(module.T.S):
+            if hasattr(layer, "weight"):
+                self.epoch_weights[ii].append(copy.deepcopy(layer.weight))
